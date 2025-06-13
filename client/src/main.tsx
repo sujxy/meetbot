cb191f3944ce482a3b5663dace1e2d3cf6f31426
sujxy <sujay052003@gmail.com>
2025-06-13 23:11:53 +0530

    Updates for main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,11 +7,7 @@ import UserProvider from "./context/userContext.tsx";
 import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
-  <GoogleOAuthProvider
-    clientId={
-      "446953567556-k9179dbk9rmutlar9e44bj9rg2p9arek.apps.googleusercontent.com"
-    }
-  >
+  <GoogleOAuthProvider clientId={""}>
     <UserProvider>
       <Toaster position={"top-right"} />
       <BrowserRouter>
